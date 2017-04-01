931b889b67808bb56b15705728b138f229c79a72
Pmc Dev <phoomparin@gmail.com>
2017-04-01 17:11:03 +0700

    :sparkles: LINE Integration Skeleton

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 import debug from "./debug"
+import line from "./line"
 import graphql from "./graphql"
 
 export default function services() {
   this.configure(debug)
+  this.configure(line)
   this.configure(graphql)
 }
---
+++ src/services/line.js
@@ -0,0 +1,49 @@
+import request from "request"
+
+// API Secrets
+// TODO: Move that to Environment Variables, but I ain't got no time
+
+const CHANNEL_ID = "1508478196"
+const CHANNEL_SECRET = "ffe7d065e984a1094792d689bd2fd0e0"
+const CHANNEL_TOKEN = "UuQd6RmSRQppG/0g6xc4Xh9GVigTR0/CqBV7afFJpYbRoMp0TGP00T1UNbEZmeDgM5VMEYF+ucWkGJwHwVoQP0cGzhAeg0RCToj8c2Pk2qCZI1/wkh6tTWlOtt9uFIMwKiqj5N/3bTCjfII3HQki3QdB04t89/1O/w1cDnyilFU="
+const TEST_USER_ID = "U33084216cc1a00f7aa27632bec769356"
+
+// SDK for interfacing with the LINE Messaging API
+// NOTE: Why the heck are the Official SDKs deprecated? Like, seriously dude? -.-
+
+class LineMessaging {
+
+  constructor(id, secret) {
+    this._id = id
+    this._secret = secret;
+  }
+
+  sendMessage() {
+
+  }
+
+}
+
+class ChatStage {
+  // TODO: We need to store user's stage in there. I'm thinking of Redis, but fuck it I'm tired.
+  //       It's a hackathon, so yeah.
+}
+
+const bot = new LineMessaging(CHANNEL_ID, CHANNEL_SECRET)
+
+class LineService {
+
+  setup(app) {
+    this.app = app
+  }
+
+  find = () => Promise.resolve({
+    data: "LINE Integration Service is Ready!"
+  })
+
+  // get = () => Promise.resolve({data: "Hello World"})
+}
+
+export default function debug() {
+  this.use("line", new LineService())
+}
